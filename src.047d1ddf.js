89b2e82531637253b45c5856a74572d3c1ee3073
Tripoli80 <35936980+Tripoli80@users.noreply.github.com>
2022-04-03 16:06:03 +0000

    Deploying to gh-pages from @ Tripoli80/WorkParcel@daa32bb9de733c7db4da39621b824a8a09c97cdf 🚀

--- src.047d1ddf.js
+++ src.047d1ddf.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/WorkParcel/src.89887937.js.map
+//# sourceMappingURL=/WorkParcel/src.047d1ddf.js.map
